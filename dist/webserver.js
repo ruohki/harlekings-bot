d1e5973ee5e8cb460d8a16b1a4c9705352098026
ruohki <ruohki@gmail.com>
2017-06-21 23:11:53 +0200

    changes

--- dist/webserver.js
+++ dist/webserver.js
@@ -30,6 +30,10 @@ var _util = require('./util.js');
 
 var _models = require('./models.js');
 
+var _discord = require('discord.js');
+
+var _discord2 = _interopRequireDefault(_discord);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
@@ -56,18 +60,51 @@ class WebServer {
         // Error Handling
         if (!this.app) throw new Error('WebServer nicht Initialisiert');
 
-        this.app.post('/api', (() => {
-            var _ref = _asyncToGenerator(function* (req, res) {
-                let { targetUser, targetMessage, passWord } = req.body;
+        this.app.post('/messageUser', (req, res) => {
+            let { targetUser, targetMessage, passWord } = req.body;
 
+            if (passWord !== "FroschFotze") {
+                return res.json({ status: false });
+            }
+            try {
+                this.bot.Client.fetchUser(targetUser, false).then(user => {
+                    /*console.log(user);
+                    const dmChannel = user.createDM();*/
+                    user.send(targetMessage).then(message => console.log(`Sent message: ${message.content}`)).catch(console.error);
+
+                    return res.json({ status: true });
+                });
+            } catch (err) {
+                (0, _util.LogMessage)(err);
+            }
+        });
+
+        this.app.post('/messageChannel', (req, res) => {
+            let { targetChannel, targetMessage, passWord } = req.body;
+
+            if (passWord !== "FroschFotze") {
+                return res.json({ status: false });
+            }
+            let Channel = this.bot.Client.guilds.get('252815455096406017').channels.get(targetChannel);
+            Channel.send(targetMessage);
+            return res.json({ status: true });
+        });
+
+        this.app.post('/joinBewerber', (() => {
+            var _ref = _asyncToGenerator(function* (req, res) {
+                let { targetUser, token, passWord } = req.body;
                 if (passWord !== "FroschFotze") {
                     return res.json({ status: false });
                 }
-
-                let user = yield _this.bot.Client.fetchUser(targetUser);
-                user.send(targetMessage);
-
-                return res.json({ status: true });
+                try {
+                    let guild = yield _this.bot.Client.guilds.get('252815455096406017');
+                    let user = yield _this.bot.Client.fetchUser(targetUser, false);
+                    let guildMember = yield guild.addMember(user, { accessToken: token });
+                    let roleResult = yield guildMember.addRole('272847394410856448');
+                    return res.json({ status: roleResult });
+                } catch (err) {
+                    (0, _util.LogMessage)(err);
+                }
             });
 
             return function (_x, _x2) {
